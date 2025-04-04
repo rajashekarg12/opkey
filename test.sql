9c696a1f27cb96d22b747604780b767697f99c30
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-04-04 11:33:42 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,3 +1,5 @@
+    drop table atStudent;
+
 create table atStudent (
     student_id number primary key,
     name varchar2(100),
@@ -8,6 +10,5 @@ create table atStudent (
 insert into atStudent (student_id, name, age, grade)
 values (1, 'John Doe', 20, 'A');
 
-select * from atStudent;d
-    drop table atStudent;
+select * from atStudent;
 
