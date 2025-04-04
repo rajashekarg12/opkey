d961f0b9c7b044bb1a43a3825ca6ccc281bf01c2
rajashekarg12 <110624878+rajashekarg12@users.noreply.github.com>
2025-04-04 11:29:36 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,13 +1,13 @@
-create table student (
+create table student1 (
     student_id number primary key,
     name varchar2(100),
     age number,
     grade varchar2(10)
 );
 
-insert into student (student_id, name, age, grade)
+insert into student1 (student_id, name, age, grade)
 values (1, 'John Doe', 20, 'A');
 
-select * from student;
+select * from student;drop table student;
+drop table student;
 
-delete from student where student_id = 1;
